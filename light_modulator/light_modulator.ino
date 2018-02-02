ec74969cc531af4f8989561d6bf169d81de5ed59
Arunima08 <23697846+arunima811@users.noreply.github.com>
2018-02-03 01:02:54 +0530

    Add files via upload

---
+++ light_modulator/light_modulator.ino
@@ -0,0 +1,29 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  for(int i=2;i<8;++i)
+  {
+    pinMode(i,OUTPUT);
+    digitalWrite(i,0);
+  }
+  
+  pinMode(A3,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = analogRead(A3);
+  val = 1023- val;
+  Serial.println(val);
+  int number = (val/170.6) + 2;
+  for(int i=2;i<=7;++i)
+  {
+    if(i<=number)
+      digitalWrite(i,1);
+    else
+      digitalWrite(i,0);
+  }
+ delay(100); 
+}
+
